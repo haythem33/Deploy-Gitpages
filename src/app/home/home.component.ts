d8340b4c3ffe8ba5e893e0134af2a1a3f72deee5
haythem33 <42939839+haythem33@users.noreply.github.com>
2019-01-11 10:31:08 +0100

    register login front

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  consultant: FormGroup;
+  company: FormGroup;
+  constructor() {
+    this.consultant = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.minLength(2) ,  Validators.maxLength(19)]),
+      email: new FormControl ('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+    });
+    this.company = new FormGroup({
+     companyname: new FormControl('', [Validators.required, Validators.minLength(2) ,  Validators.maxLength(19)]),
+     companyemail: new FormControl('', [Validators.required, Validators.email]),
+     companypassword: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+    });
+  }
 
   ngOnInit() {
-}
+  }
+
 
 
 }
